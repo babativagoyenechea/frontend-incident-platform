d08263b4bd8212a27342afb669c93abb28e83ab1
Andrés Babativa <babativagoyenechea@gmail.com>
2026-06-21 14:30:00 -0500

    feat: build modular presentation layer and monochrome telemetry console

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DashboardProvider } from './context/DashboardContext';
+import { Dashboard } from './pages/Dashboard';
+
+export default function App() {
+  return (
+    <DashboardProvider>
+      <Dashboard />
+    </DashboardProvider>
+  );
+}
